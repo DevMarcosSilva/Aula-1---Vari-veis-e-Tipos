23fb465291ed3aa0923b70a21cc0b1b140a8f860
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-18 17:42:26 -0300

    ajuste Ex 4

--- Ex 4.py
+++ Ex 4.py
@@ -5,7 +5,7 @@
 numero = input('Informe um numero com 5 digitos: ')
 
 def separa_digitos():
-  for i in range(0,1):
-       print(numero[i],numero[i+1],numero[i+2],numero[i+3],numero[i+4])
+  for i in range(0,len(numero)):
+       print(numero[i],end="  ")
 
 separa_digitos()
