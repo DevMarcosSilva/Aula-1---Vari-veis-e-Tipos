f7fd246fd973c423f64454649e9b8bda6a134b54
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-18 18:39:30 -0300

    ajuste função

--- Ex 5!.py
+++ Ex 5!.py
@@ -1,17 +1,25 @@
 #Escreva um aplicativo que receba a, b e c, coeficientes de uma equação do segundo grau, 
 # e calcule as raízes x’ e x” através da fórmula de Báskara.
+import math
 
-a = float(input('informe A: '))
-b = float(input('informe B: '))
-c = float(input('informe C: '))
+a = int(input('informe A: '))
+b = int(input('informe B: '))
+c = int(input('informe C: '))
 
-#def calculo_x1():
- #   x1=-b+
     
 def delta():
     delta = (b**2)-(4*a*c)
-    print(delta)
+    return delta
 
-delta()
+def calculo_x1():
+    x1= (-b + math.sqrt(delta())) / (2 * a)
+    return x1
 
+def calculo_x2():
+   x2= -b-math.sqrt(delta())/2*a
+   return x2
+
+print("x1:", delta())
+print("x1:", calculo_x1())
+print("x2:", calculo_x2())
     
