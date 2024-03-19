c5746732145ef54ad442db4b8f9fa6bf10406159
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-19 19:38:26 -0300

    ajuste Ex 2

--- Ex 2!.py
+++ Ex 2!.py
@@ -1,4 +1,38 @@
 #Escreva um aplicativo que exibe uma caixa, uma oval,
 # uma seta e um losango utilizando asteriscos (*).
 
-print('* '*5,'*\n'*5)
+# Desenhar uma caixa
+print("Caixa:")
+print("*****")
+print("*   *")
+print("*   *")
+print("*   *")
+print("*****")
+
+# Desenhar uma oval
+print("\nOval:")
+print("  ***  ")
+print(" *   * ")
+print("*     *")
+print(" *   * ")
+print("  ***  ")
+
+# Desenhar uma seta
+# Desenhar uma seta
+print("\nSeta:")
+print("  *  ")
+print(" *** ")
+print("*****")
+print("  *  ")
+print("  *  ")
+
+
+# Desenhar um losango
+print("\nLosango:")
+print("   *   ")
+print("  ***  ")
+print(" ***** ")
+print("  ***  ")
+print("   *   ")
+
+
