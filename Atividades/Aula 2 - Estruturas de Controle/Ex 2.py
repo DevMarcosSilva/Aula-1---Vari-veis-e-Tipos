9c750fdaea10a7505586a8c468b24d6b295f5097
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-23 14:59:23 -0300

    numero primo

--- Atividades/Aula 2 - Estruturas de Controle/Ex 2.py
+++ Atividades/Aula 2 - Estruturas de Controle/Ex 2.py
@@ -1,2 +1,9 @@
 #Escreva um programa que leia um número e verifique se é ou não um
 #número primo.
+
+numero = int(input('Informe um numero: '))
+
+if numero > 1 and numero % numero == 0 and numero % 1 == 0 and numero %2 !=0: 
+    print(f'{numero} é primo!')
+else:
+    print(f'{numero} NÃO é primo!')    
