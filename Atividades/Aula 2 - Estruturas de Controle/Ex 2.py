afe6ae7434733f89aa2523f788c6f60ccb6f430f
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-23 12:44:25 -0300

    Inserindo enunciados

---
+++ Atividades/Aula 2 - Estruturas de Controle/Ex 2.py
@@ -0,0 +1,2 @@
+#Escreva um programa que leia um número e verifique se é ou não um
+#número primo.
---
+++ Atividades/Aula 2 - Estruturas de Controle/Ex 4.py
@@ -0,0 +1,7 @@
+#O fatorial de um inteiro não negativo n é escrito como n! (pronuncia-se “n
+#fatorial”) e é definido como segue:
+#n! = n · (n – 1) · (n – 2) · ... · 1 (para valores de n maiores ou iguais a 1) e n! =
+#1 (para n = 0)
+#Por exemplo, 5! = 5 · 4 · 3 · 2 · 1, o que dá 120.
+#Escreva um aplicativo que lê um inteiro não negativo, calcula e imprime seu
+#fatorial.
