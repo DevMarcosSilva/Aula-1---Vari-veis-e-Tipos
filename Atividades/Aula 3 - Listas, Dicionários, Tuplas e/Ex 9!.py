9d073164a7e3a699f5df74f007900fa7dee27e55
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-30 17:11:03 -0300

    Ex 9

--- Atividades/Aula 3 - Listas, Dicionários, Tuplas e/Ex 9!.py
+++ Atividades/Aula 3 - Listas, Dicionários, Tuplas e/Ex 9!.py
@@ -1,2 +1,34 @@
-#faça um jogo da Forca utilizando listas. Dada uma palavra, dê algumas
-#chances para o usuário acertar.
+#Escreva um programa que compare duas listas. Considere a primeira lista
+#como a versão inicial e a segunda como a versão após alterações.
+#Utilizando operações com conjuntos, seu programa deverá imprimir a lista
+#de modificações entre essas duas versões, listando:
+# ◦ os elementos que não mudaram
+# ◦ os novos elementos
+# ◦ os elementos que foram removidos
+
+def comparar_listas(lista_inicial, lista_alterada):
+    
+    conjunto_inicial = set(lista_inicial)
+    conjunto_alterado = set(lista_alterada)
+    
+    
+    iguais = conjunto_inicial.intersection(conjunto_alterado)
+    
+    
+    novos = conjunto_alterado - conjunto_inicial
+    
+    
+    removidos = conjunto_inicial - conjunto_alterado
+    
+
+    print("Elementos que não mudaram:")
+    print(iguais)
+    print("\nNovos elementos:")
+    print(novos)
+    print("\nElementos removidos:")
+    print(removidos)
+
+lista_inicial = [1, 2, 3, 4, 5]
+lista_alterada = [2, 3, 5, 6, 7]
+comparar_listas(lista_inicial, lista_alterada)
+
