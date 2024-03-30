c9b06fe53242005018e8dd0f5b7096e8f4d54f2b
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-30 10:47:01 -0300

    Ex 5 concluído

--- Atividades/Aula 3 - Listas, Dicionários, Tuplas e/Ex 5.py
+++ Atividades/Aula 3 - Listas, Dicionários, Tuplas e/Ex 5.py
@@ -1,2 +1,17 @@
-#faça um jogo da Forca utilizando listas. Dada uma palavra, dê algumas
-#chances para o usuário acertar.
+#Escreva um programa que copie os valores pares para uma lista e os
+#valores ímpares para outra lista. A lista inicialmente de valores é V=[9,8,7,12,0,13,21] .
+
+lista = [9,8,7,12,0,13,21]
+par = []
+impar = []
+
+def num_par_e_num_impar():
+    for i in lista:
+        if i % 2 == 0:
+            par.append(i)
+        else:
+            impar.append(i)              
+    
+num_par_e_num_impar()
+print(f'Os numeros pares da lista são: ',par)
+print(f'Os numeros impares da lista são: ',impar)          
