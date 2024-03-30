1bd59b748ee07fe16faeff5e4c160767e66305c4
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-03-30 10:12:58 -0300

    criando todos os exercicios

---
+++ Atividades/Aula 3 - Listas, Dicionários, Tuplas e/Ex 10.py
@@ -0,0 +1,2 @@
+#faça um jogo da Forca utilizando listas. Dada uma palavra, dê algumas
+#chances para o usuário acertar.
--- Atividades/Aula 3 - Listas, Dicionários, Tuplas e/tes.py
+++
@@ -1,5 +0,0 @@
-vetor = [1, 2, 3, 4, 5]
-
-for i in range(len(vetor)):
-    elemento = vetor[i]
-    print("Elemento no índice", i, ":", elemento)
