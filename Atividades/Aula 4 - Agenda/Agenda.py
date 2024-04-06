2038ec707f8ac2f0c53d235ca9aedab689a36ea9
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-04-06 19:53:07 -0300

    atl inserir

--- Atividades/Aula 4 - Agenda/Agenda.py
+++ Atividades/Aula 4 - Agenda/Agenda.py
@@ -10,13 +10,13 @@
 #f. Permita o cadastro de tipos de telefone: celular, fixo, residência ou
 #trabalho
 import time
-
+numero_Tarefas = [10]
 nomeTarefa = []
 dataInicio = []
 dataTerm = []
-
+i = 0
 def menu():
-    opcao = int(input('Olá informe o que deseja!\n 1.Inserir nova tarefa\n 2.editar tarefa\n 3.visualiza todas as tarefas\n 4.Apagar tarefa\n 4.Sair da agenda\n'))
+    opcao = int(input('Olá informe o que deseja!\n 1.Inserir nova tarefa\n 2.editar tarefa\n 3.visualiza todas as tarefas\n 4.Apagar tarefa\n 5.Sair da agenda\n'))
     while opcao != 1 and opcao != 2 and opcao != 3 and opcao != 4 and opcao != 5 :
        print('Opçao inválida insira um numero de 1 a 5\n')
        time.sleep(1)
@@ -26,25 +26,30 @@ def menu():
     elif opcao == 2:
         listadeTarefas()          
 
-def inserir():
-    nomeinserido = {}
-    nomeinserido['nome-tarefa'] = input('Informe o nome da tarefa que deseja inserir: ')
-    nomeTarefa.append(nomeinserido)
-
-    dataInicioUser = {}
-    dataInicioUser['data-inicio'] = input('Informe a data de inicio da tarefa: ')
-    dataInicio.append(dataInicioUser)
+def inserir(i=0):
     
-    dataTermUser = {}
-    dataTermUser['data-termino'] = input('Informe a data de término da tarefa: ')
-    dataTerm.append(dataTermUser)
+    while i < len(numero_Tarefas):
+        nomeinserido = {}
+        nomeinserido['nome-tarefa'] = input('Informe o título da tarefa que deseja inserir: ')
+        nomeTarefa.append(nomeinserido)
 
-    print('Tarefa agendada com sucesso!')
-    opcao = int(input('para cadastrar mais uma digite 1'))
+        dataInicioUser = {}
+        dataInicioUser['data-inicio'] = input('Informe a data de inicio da tarefa: ')
+        dataInicio.append(dataInicioUser)
+        
+        dataTermUser = {}
+        dataTermUser['data-termino'] = input('Informe a data de término da tarefa: ')
+        dataTerm.append(dataTermUser)
+
+        print(f'Tarefa agendada com sucesso! Nº TAREFA É: {i+1}')
+        i += 1
+        
+    
+    opcao = int(input('Para cadastrar mais uma tarefa digite 1 ou 2 para retornar ao menu principal'))
     if opcao == 1:
         inserir()
-    if opcao == 2:
-        listadeTarefas()    
+    elif opcao == 2:
+        menu()    
 
 def listadeTarefas():
     for i in range(0,len(nomeTarefa)):
