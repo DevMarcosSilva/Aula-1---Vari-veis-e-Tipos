d42785c319d7992835f8475a18d902032ce55347
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-04-27 17:11:42 -0300

    ajustes

--- Atividades/Aula 6 - POO/classes.py
+++ Atividades/Aula 6 - POO/classes.py
@@ -1,19 +1,20 @@
 class ContaBancaria:
     def  __init__(self,numero_agencia,tipo_conta,saldo,limite):
         self.numero_agencia = numero_agencia
-        self.tip_conta = tipo_conta
+        self.tipo_conta = tipo_conta
+        self.Lista_tip_conta = ['Corrente','Poupança','Salário']
+        if self.tipo_conta in self.Lista_tip_conta:
+            self.tipo_conta == self.Lista_tip_conta
+        else:
+            raise Exception('conta invalida')       
         self.saldo = saldo
         self.limite = limite
     
     def consultar_saldo(self):
         print(f"O saldo é: {self.saldo}")    
         
- 
- 
- 
-Conta = input(f'informe o num da agen ',ContaBancaria())
 
-print(Conta.saldo)    
+
 '''
  def sacar(self,valor):
         return "o valor sacado foi {}\n seu saldo agora é {}".format(self.saldo)
@@ -31,8 +32,6 @@ print(Conta.saldo)
         #terminar
     
     def fechar_conta(self) :     
-      
+'''      
+                
         
-class historico:
-    d        
-'''
---
+++ Atividades/Aula 6 - POO/main.py
@@ -0,0 +1,13 @@
+from classes import *
+        
+ 
+num_agen = int(input(f'infome o num da age')) 
+Conta = ContaBancaria(num_agen,'Corrente',2,3,)
+
+print(Conta.numero_agencia,Conta.tipo_conta,Conta.saldo,Conta.limite)
+     
+
+        
+
+
+
