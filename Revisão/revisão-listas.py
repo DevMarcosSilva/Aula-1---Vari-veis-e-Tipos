878ab3b66c04a2d75b77c48dbed93c5e4da59a89
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-05-01 18:53:13 -0300

    finalizado

--- Revisão/revisão-listas.py
+++ Revisão/revisão-listas.py
@@ -5,6 +5,10 @@ with open('listas.txt','r',encoding="utf-8") as arquivo:
     lista3=set(dados[3].split())
     lista4=set(dados[4].split())
     lista5=set(dados[5].split())
+
+
+
+    
        
 elemen_n_repetem=(lista1^lista2^lista3^lista4^lista5)-(lista1&lista2&lista3&lista4&lista5)
 
@@ -24,11 +28,14 @@ for elemento in todos_elementos:
     
     contagem[elemento] = contagem.get(elemento, 0) + (1 if elemento in lista5 else 0)
 
-print('Elementos que aparecem uma única vez: ',elemen_n_repetem)
-print('-----------------------------')
-print('Repetições: ')
-for elemento, ocorrencias in contagem.items():
-    print(f"'{elemento}', se repete: {ocorrencias}")
-print('-----------------------------')
-print('\nElementos presentes na última lista mas não na primeira: ',lista5-lista1)
-print('\n')
+with open('listasSaida.txt','w',encoding="utf-8") as arquivoSaida:
+    arquivoSaida.write(f'Elementos que aparecem uma única vez: {elemen_n_repetem}')
+    arquivoSaida.write('\n-----------------------------\n')
+    arquivoSaida.write('Repetições: \n')
+    for elemento, ocorrencias in contagem.items():
+        arquivoSaida.write(f"'{elemento}', se repete: {ocorrencias}\n")
+    arquivoSaida.write('-----------------------------')
+    resultado = lista5-lista1
+    
+    arquivoSaida.write(f'\nElementos presentes na última lista mas não na primeira: {resultado}')
+    
