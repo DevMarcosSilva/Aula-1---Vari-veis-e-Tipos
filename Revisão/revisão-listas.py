70cc372a3314d3724c8577d3f04c0e5134f01c3b
Dev Marcos Silva <mvinicius17112002@gmail.com>
2024-05-01 15:48:19 -0300

    conuntos SET

--- Revisão/revisão-listas.py
+++ Revisão/revisão-listas.py
@@ -1,7 +1,14 @@
-N = int(input()) 
-
-notas = [100.00,50.00,20.00,10.00,5.00,2.00]
-print ("R$ %d:"%(N))
-for x in notas:
-    print ("%i nota(s) de R$ %.2f"%((N/x),x))
-    N %= x
+with open('listas.txt','r',encoding="utf-8") as arquivo:
+    dados = arquivo.readlines()
+    lista1=set(dados[1].split())
+    lista2=set(dados[2].split())
+    lista3=set(dados[3].split())
+    lista4=set(dados[4].split())
+    lista5=set(dados[5].split())
+    
+    
+    '''teste = set(lista3)
+    teste2 = set(lista4)
+    
+    result= teste.intersection(teste2)'''
+    print(lista1 | lista2)
